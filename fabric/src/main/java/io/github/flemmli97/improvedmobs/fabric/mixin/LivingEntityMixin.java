5d9d289d254dd58a4bd77ba363c9269603fccf14
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-11-30 15:39:42 +0100

    fix projectile damage inc not applying to player fix #267 fix #288

--- fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/mixin/LivingEntityMixin.java
+++ fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/mixin/LivingEntityMixin.java
@@ -3,13 +3,14 @@ package io.github.flemmli97.improvedmobs.fabric.mixin;
 import io.github.flemmli97.improvedmobs.events.EventCalls;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin({LivingEntity.class})
+@Mixin({LivingEntity.class, Player.class})
 public abstract class LivingEntityMixin {
 
     @ModifyVariable(method = "actuallyHurt", at = @At(value = "HEAD"), argsOnly = true)
